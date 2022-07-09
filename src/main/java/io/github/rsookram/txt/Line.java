6e56da5f381f56129eac0188793e246183119ac2
rsookram <rashadsookram@gmail.com>
2022-07-08 20:53:56 -0400

    Document entity classes

--- src/main/java/io/github/rsookram/txt/Line.java
+++ src/main/java/io/github/rsookram/txt/Line.java
@@ -1,8 +1,18 @@
 package io.github.rsookram.txt;
 
+/**
+ * A line of {@link Text} from a file. This may be long enough to be rendered across multiple lines.
+ */
 public class Line {
 
+    /**
+     * The text to display for this line, without any leading or trailing whitespace.
+     */
     public final String text;
+    /**
+     * The offset (measured in characters, zero-indexed) of the first character in {@link #text}
+     * relative to the start of the file containing this line.
+     */
     public final int offset;
 
     public Line(String text, int offset) {
--- src/main/java/io/github/rsookram/txt/Text.java
+++ src/main/java/io/github/rsookram/txt/Text.java
@@ -2,9 +2,18 @@ package io.github.rsookram.txt;
 
 import java.util.List;
 
+/**
+ * The text to display for a single file.
+ */
 public class Text {
 
+    /**
+     * The {@link Line}s containing the text to display.
+     */
     public final List<Line> lines;
+    /**
+     * The total number of characters in {@link #lines}.
+     */
     public final int length;
 
     public Text(List<Line> lines, int length) {
